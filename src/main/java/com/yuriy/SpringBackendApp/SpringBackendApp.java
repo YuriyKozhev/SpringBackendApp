4133f45b9ba2a00f00eb28d14520f336945b5c1d
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-27 23:42:55 +0300

    add template Spring project files

---
+++ src/main/java/com/yuriy/SpringBackendApp/SpringBackendApp.java
@@ -0,0 +1,13 @@
+package com.yuriy.SpringBackendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuriy/SpringBackendApp/SpringBackendAppTests.java
@@ -0,0 +1,13 @@
+package com.yuriy.SpringBackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
