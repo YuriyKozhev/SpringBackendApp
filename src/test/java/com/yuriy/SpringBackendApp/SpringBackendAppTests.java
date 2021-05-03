fa0c260309a97574c7508aac56a194e99a9d0085
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-03 18:30:08 +0300

    add overall app test with context loading

--- src/test/java/com/yuriy/SpringBackendApp/SpringBackendAppTests.java
+++ src/test/java/com/yuriy/SpringBackendApp/SpringBackendAppTests.java
@@ -1,13 +1,21 @@
 package com.yuriy.SpringBackendApp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-class SpringBackendAppTests {
+public class SpringBackendAppTests {
+
+	@Autowired
+	private Controller controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
+
 
 }
