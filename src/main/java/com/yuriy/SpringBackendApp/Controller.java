57458cf7771f0373f95fc232bb18f250870172f9
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-05 00:29:06 +0300

    add getAllUsers controller method

--- src/main/java/com/yuriy/SpringBackendApp/Controller.java
+++ src/main/java/com/yuriy/SpringBackendApp/Controller.java
@@ -1,10 +1,17 @@
 package com.yuriy.SpringBackendApp;
 
+import com.yuriy.SpringBackendApp.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String printMain() {
@@ -14,5 +21,10 @@ public class Controller {
     @GetMapping("/greet")
     public String greet() {
         return "Hello!";
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
