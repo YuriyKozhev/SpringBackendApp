1b4877a9773d0375839f5f82ae6ef608f984b2e2
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-15 15:19:39 +0300

    fill the users table only if it is empty

--- src/main/java/com/yuriy/SpringBackendApp/LoadDatabase.java
+++ src/main/java/com/yuriy/SpringBackendApp/LoadDatabase.java
@@ -5,15 +5,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class LoadDatabase {
 
     @Bean
     CommandLineRunner initDataBase(UserRepository userRepository) {
 
+        List<User> users = userRepository.findAll();
+
         return args -> {
-            userRepository.save(new User("john.smith@example.com", "john", "smith"));
-            userRepository.save(new User("jack.smith@example.com", "jack", "smith"));
+            if (users.isEmpty()) {
+                userRepository.save(new User("john.smith@example.com", "john", "smith"));
+                userRepository.save(new User("jack.smith@example.com", "jack", "smith"));
+            }
         };
     }
 }
