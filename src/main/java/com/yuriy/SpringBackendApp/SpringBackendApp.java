e2ed88cd1e2d37d53a60b92eef880b4dd505cea4
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-07-06 00:16:45 +0300

    add logging to SpringBackendApp file

--- src/main/java/com/yuriy/SpringBackendApp/SpringBackendApp.java
+++ src/main/java/com/yuriy/SpringBackendApp/SpringBackendApp.java
@@ -1,13 +1,17 @@
 package com.yuriy.SpringBackendApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBackendApp {
+	private static final Logger logger = LoggerFactory.getLogger(SpringBackendApp.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApp.class, args);
+		logger.info("Application has been started");
 	}
 
 }
